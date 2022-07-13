18791be5aca12a81d475ec68aefdf33cccfc249c
Yasmin Rodrigues <yasminrodsil@gmail.com>
2022-07-13 16:56:59 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,6 @@
+#Programa que leia um número inteiro e mostre na tele se ele é ímpar ou par
+n = int(input('Digite um número qualquer:'))
+if n % 2 == 0:
+    print('Esse é um número PAR')
+else:
+    print('Esse é um número ÍMPAR')
